2d1bf381b18f1e1404e679b5dca4cd5b2be68cbb
FNSRKRP2024sanchez <sanchezruizfreidynoel@gmail.com>
2026-05-20 08:56:10 -0600

    feat (admin): "Dahboard"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    }
+];
+
+
